d141e323dd7d88ff9d153e3f4eb502409028b0e2
standardforever <standard.forever123@gmail.com>
2022-07-14 03:04:23 -0400

    updated

--- main.c
+++ main.c
@@ -1,67 +1,13 @@
 #include "monty.h"
-#include <stdio.h>
 
-stack_t *stack = NULL;
 
-/**
- * push - it push an element to the stack
- * @stack: the head of the stack
- * @line_number: the number of line
- * Return: Nothing
- */
-void push(stack_t **stack, unsigned int line_number)
-{
-	stack_t *newnode = (stack_t *)malloc(sizeof(stack_t));
-	int num;
-	char *tok;
-
-	tok = strtok(NULL, DELIMS);
-	if (tok == NULL || stack == NULL)
-	{
-		fprintf(stderr, "L%i: usage: push integer\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-	num = atoi(tok);
-
-	if (newnode == NULL)
-	{
-		write(STDERR, "Error: malloc failed\n", 21);
-		exit(EXIT_FAILURE);
-	}
-		
-	newnode->n = num;
-	newnode->prev = NULL;
-	newnode->next = *stack;
-	if (*stack != NULL)
-	{
-		(*stack)->prev = newnode;
-	}
-	*stack = newnode;
-
-}
 
 /**
- * pall - it print the value in a stack
- * @stack: the head of the stack 
- * @line_number: unuse varaible
- * Return: Nothing
- */
-void pall(stack_t **stack, unsigned int line_number __attribute__((unused)))
-{
-	stack_t *str = *stack;
-	while (str != NULL)
-	{
-		printf("%i\n", str->n);
-		str = str->next;
-	}
-	free(str);
-}
-
-/**
- * stack - the head of the stack
- * @tok: the commmand from the file input 
+ * process_str - it process the input file
+ * @stack:  the head of the stack
+ * @tok: the commmand from the file input
  * @line_number: the line number from the file
- * Return: Nothing 
+ * Return: Nothing
  */
 void process_str(stack_t **stack, char *tok, unsigned int line_number)
 {
@@ -72,8 +18,9 @@ void process_str(stack_t **stack, char *tok, unsigned int line_number)
 		{"pop", pop},
 		{NULL, NULL}
 	};
-	
+
 	int i;
+
 	for (i = 0; instructions[i].opcode; i++)
 	{
 		if (strcmp(instructions[i].opcode, tok) == 0)
@@ -89,35 +36,8 @@ void process_str(stack_t **stack, char *tok, unsigned int line_number)
 	}
 }
 
-/**
- * pint - it print the value at the top of the stack
- * @stack: the head of the stack
- * @line_number: the line from the file
- * Return: Nothing
- */
-void pint(stack_t **stack, unsigned int line_number)
-{
-	if (*stack != NULL)
-		printf("%i\n", (*stack)->n);
-	else
-	{
-		fprintf(stderr, "L%i: can't pint, stack empty\n", line_number);
-		exit(EXIT_FAILURE);
-	}	
-}
 
 
-void pop(stack_t **stack, unsigned int line_number)
-{
-	stack_t *temp = *stack;
-	if (*stack == NULL)
-	{
-		fprintf(stderr, "L%i: can't pop an empty stack\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-	(*stack) = (*stack)->next;
-	free(temp);
-}
 /**
  * main - contains the main function
  * @argc: the number of input from the commmand line
@@ -131,7 +51,7 @@ int main(int argc, char *argv[])
 	size_t len = 0;
 	ssize_t read = 0;
 	int line_number = 1;
-	
+	stack_t *stack = NULL;
 
 	if (argc != 2)
 	{
@@ -145,12 +65,12 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	while ((read = getline(&line, &len, fp))!= - 1)
+	while ((read = getline(&line, &len, fp)) != -1)
 	{
 		tok = strtok(line, DELIMS);
 		process_str(&stack, tok, line_number);
 		line_number++;
 	}
-	
+
 	return (0);
 }
--- monty.h
+++ monty.h
@@ -45,45 +45,15 @@ typedef struct instruction_s
 extern stack_t *stack;
 
 
-#define INSTRUCTIONS { \
-		{"push", push},\
-		{"pall", pall},\
-		{"pint", pint},\
-		{"pop", pop},\
-		{"swap", swap},\
-		{"nop", nop},\
-		{"div", _div},\
-		{"mul", _mul},\
-		{"add", _add},\
-		{"sub", _sub},\
-		{"mod", mod},\
-		{"pchar", pchar},\
-		{"pstr", pstr},\
-		{"rotl", rotl},\
-		{"rotr", rotr},\
-		{NULL, NULL} \
-	}
-/**
-* struct help - argument for the current opcode
-* @data_struct: stack mode, stack (default) and queue
-* @argument: the arguments of the string
-*
-* Description: global structure used to pass data around the functions easily
-*/
-typedef struct help
-{
-	int data_struct;
-	char *argument;
-} help;
-help global;
 
-extern int status;
-
+/* instructions.c */
 void push(stack_t **stack, unsigned int line_cnt);
 void pall(stack_t **stack, unsigned int line_cnt);
 void pint(stack_t **stack, unsigned int line_cnt);
 void swap(stack_t **stack, unsigned int line_cnt);
 void pop(stack_t **stack, unsigned int line_cnt);
+
+
 void nop(stack_t **stack, unsigned int line_cnt);
 void _div(stack_t **stack, unsigned int line_cnt);
 void _add(stack_t **stack, unsigned int line_cnt);
@@ -95,6 +65,11 @@ void pstr(stack_t **stack, unsigned int line_cnt);
 void rotl(stack_t **stack, unsigned int line_count);
 void rotr(stack_t **stack, unsigned int line_count);
 void opcode(stack_t **stack, char *str, unsigned int line_cnt);
+
+/* main.c */
+void process_str(stack_t **stack, char *tok, unsigned int line_number);
+int main(int argc, char *argv[]);
+
 
 int is_digit(char *string);
 int isnumber(char *str);
